1e79dbd0092ac2501b7de62b5c5f1543dfd55370
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-09-22 13:15:20 +0300

    Apply autoexporter

--- library/ModelieroBase/Classes.hs
+++ library/ModelieroBase/Classes.hs
@@ -1,10 +1,1 @@
-module ModelieroBase.Classes
-  ( module ModelieroBase.Classes.Special,
-    module ModelieroBase.Classes.Anonymizable,
-    module ModelieroBase.Classes.Literal,
-  )
-where
-
-import ModelieroBase.Classes.Anonymizable
-import ModelieroBase.Classes.Literal
-import ModelieroBase.Classes.Special
+{-# OPTIONS_GHC -F -pgmF autoexporter #-}
--- library/ModelieroBase/Data.hs
+++ library/ModelieroBase/Data.hs
@@ -1,18 +1,1 @@
-module ModelieroBase.Data
-  ( module ModelieroBase.Data.Email,
-    module ModelieroBase.Data.Hostname,
-    module ModelieroBase.Data.IpV4,
-    module ModelieroBase.Data.IpV6,
-    module ModelieroBase.Data.Iri,
-    module ModelieroBase.Data.Iso8601DateTime,
-    module ModelieroBase.Data.Uuid,
-  )
-where
-
-import ModelieroBase.Data.Email
-import ModelieroBase.Data.Hostname
-import ModelieroBase.Data.IpV4
-import ModelieroBase.Data.IpV6
-import ModelieroBase.Data.Iri
-import ModelieroBase.Data.Iso8601DateTime
-import ModelieroBase.Data.Uuid
+{-# OPTIONS_GHC -F -pgmF autoexporter #-}
